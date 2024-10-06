b4a486d9466352eeb551ec3d1f49180ce5b67cd1
LeDoanTrung <86414540+LeDoanTrung@users.noreply.github.com>
2024-10-06 18:02:28 +0700

    Input test with Toggle button

---
+++ src/main/java/test/apiLearning/GetValue.java
@@ -0,0 +1,34 @@
+package test.apiLearning;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import org.openqa.selenium.WebElement;
+import utils.AppiumDriverEx;
+
+public class GetValue {
+    public static void main(String[] args) {
+        //Driver Instance
+        AppiumDriver appiumDriver = AppiumDriverEx.getAppiumDriver();
+
+        //Set Desired Capabilities to send to Appium Server
+        WebElement loginLabel = appiumDriver.findElement(MobileBy.accessibilityId("Login"));
+        loginLabel.click();
+
+        WebElement username = appiumDriver.findElement(MobileBy.AccessibilityId("input-email"));
+        username.sendKeys("trungletest@gmail.com");
+
+        WebElement password = appiumDriver.findElement(MobileBy.AccessibilityId("input-password"));
+        password.sendKeys("123456789");
+
+        WebElement loginButton = appiumDriver.findElement(MobileBy.AccessibilityId("button-LOGIN"));
+        loginButton.click();
+
+        //Get the title form the dialog
+        WebElement successTitle = appiumDriver.findElement(MobileBy.xpath("//android.widget.TextView[@text='Success']"));
+        if (successTitle.isDisplayed() && "Success".equals(successTitle.getText())) {
+            System.out.println("Dialog title is displayed and the text is 'Success'");
+        } else {
+            System.out.println("Dialog title is not displayed or the text is not 'Success'");
+        }
+    }
+}
--- src/main/java/utils/AppiumDriverEx.java
+++ src/main/java/utils/AppiumDriverEx.java
@@ -2,7 +2,6 @@ package utils;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileBy;
-import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -18,28 +17,20 @@ public class AppiumDriverEx {
 
         //Set Desired Capabilities to send to Appium Server
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
-        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5554");
-        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11");
-        desiredCapabilities.setCapability("appPackage", "com.wdiodemoapp");
-        desiredCapabilities.setCapability("appActivity", "com.wdiodemoapp.MainActivity");
+        desiredCapabilities.setCapability(MobileCapabilityTypeEx.PLATFORM_NAME, "Android");
+        desiredCapabilities.setCapability(MobileCapabilityTypeEx.AUTOMATION_NAME, "UiAutomator2");
+        desiredCapabilities.setCapability(MobileCapabilityTypeEx.DEVICE_NAME, "emulator-5554");
+        desiredCapabilities.setCapability(MobileCapabilityTypeEx.PLATFORM_VERSION, "11");
+        desiredCapabilities.setCapability(MobileCapabilityTypeEx.APP_PACKAGE, "com.wdiodemoapp");
+        desiredCapabilities.setCapability(MobileCapabilityTypeEx.APP_ACTIVITY, "com.wdiodemoapp.MainActivity");
+        desiredCapabilities.setCapability(MobileCapabilityTypeEx.NEW_COMMAND_TIMEOUT, "60");
 
         //Setup the APpium Server URL to connect
         try {
-            URL appiumServerURL = new URL("http://192.168.217.22:4723");
+            URL appiumServerURL = new URL("http://localhost:4723");
 
             appiumDriver = new AppiumDriver(appiumServerURL, desiredCapabilities);
             appiumDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
-            WebElement loginLabel = appiumDriver.findElement(MobileBy.accessibilityId("Login"));
-            loginLabel.click();
-
-            WebElement username = appiumDriver.findElement(MobileBy.AccessibilityId("input-email"));
-            username.sendKeys("trungletest@gmail.com");
-
-            WebElement password = appiumDriver.findElement(MobileBy.AccessibilityId("input-password"));
-            password.sendKeys("123456");
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
